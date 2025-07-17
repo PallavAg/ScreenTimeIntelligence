677c48676354abfe2ce36ba7e272c0c64f0f43b9
Pallav Agarwal <agpallav@gmail.com>
2025-07-16 21:18:57 -0400

    Initial Commit

---
+++ ScreenTimeIntelligence/ScreenTimeIntelligenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeIntelligenceApp.swift
+//  ScreenTimeIntelligence
+//
+//  Created by Pallav Agarwal on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeIntelligenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
